8fe212de1d8656bdcab792bf6c578c372312e08d
unknown <Start@BS-WS-N310-002.STUD.UNV.SU>
2025-09-18 17:31:33 +0300

    uncomment

--- labs_cpp/lab1_tusk2.cpp
+++ labs_cpp/lab1_tusk2.cpp
@@ -1,23 +1,23 @@
 #include <iostream>  // Включаем библиотеку для соут
 
-//int main()
-//{
-//	setlocale(LC_ALL, "RU");
-//	int n1 = 150;
-//	float n2 = 15.993;
-//	short n3 = 250;
-//	std::cout << n3;
-//	std::cout << "n1 = " << n1 << std::endl;
-//	std::cout << "n2 = " << n2 << std::endl;
-//	std::cout << "n3 = " << n3 << std::endl;
-//
-//	short d = 29;
-//	std::string m = "Декабря";
-//	short y = 2005;
-//	std::cout << "Моя дата рождения: " << d << ' ' << m << ' ' << y << ' ' << "года" << std::endl;
-//
-//	const float n4 = 2.3;
-//	const std::string s = "WINDOWS";
-//	std::cout << n4 << ' ' << s;
-//}
-//
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	int n1 = 150;
+	float n2 = 15.993;
+	short n3 = 250;
+	std::cout << n3;
+	std::cout << "n1 = " << n1 << std::endl;
+	std::cout << "n2 = " << n2 << std::endl;
+	std::cout << "n3 = " << n3 << std::endl;
+
+	short d = 29;
+	std::string m = "Декабря";
+	short y = 2005;
+	std::cout << "Моя дата рождения: " << d << ' ' << m << ' ' << y << ' ' << "года" << std::endl;
+
+	const float n4 = 2.3;
+	const std::string s = "WINDOWS";
+	std::cout << n4 << ' ' << s;
+}
+
