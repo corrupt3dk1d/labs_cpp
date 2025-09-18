3113d0674ae50fa74d840b279cb96d5c61d59ab9
unknown <Start@BS-WS-N310-002.STUD.UNV.SU>
2025-09-18 17:22:12 +0300

    lab1

---
+++ labs_cpp/lab1_tusk1.cpp
@@ -0,0 +1,12 @@
+﻿
+
+#include <iostream>  // Включаем библиотеку для соут
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	std::cout << "First string" << std::endl;
+	std::cout << "Спецсимвол" << "\"" << "\\" << "\"";
+	
+}
+
